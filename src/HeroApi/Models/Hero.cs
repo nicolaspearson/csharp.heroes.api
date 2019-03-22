31c4c3f11d1becf437c4b2b710f3720a57b7d2fa
Nicolas Pearson <nic.s.pearson@gmail.com>
2019-03-22 20:27:27 +0200

    Added Hero Model

---
+++ src/HeroApi/Models/Hero.cs
@@ -0,0 +1,11 @@
+namespace HeroApi.Models
+{
+    public class Hero
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Identity { get; set; }
+        public string Hometown { get; set; }
+        public int Age { get; set; }
+    }
+}
